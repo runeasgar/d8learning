04bd203f434231324e79370ef08bd5fdf77bce72
Joshua Taylor <joshua.taylor@acquia.com>
2013-07-09 16:14:23 -0400

    continued work

--- lib/Drupal/test/Controller/TestController.php
+++ lib/Drupal/test/Controller/TestController.php
@@ -3,15 +3,26 @@
 namespace Drupal\test\Controller;
 
 use Drupal\test\Form\TestSettingsForm;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class TestController {
 
-  public function helloworld() {
-    $output = '<p>hello world</p>';    
+  public function helloworld($name = '') {
+    $output = '<p>hello world';
+    if (!empty($name)) {
+     	$output .= " and {$name}!</p>"; 
+    } else { 
+       $output .= '!</p>'; 
+    }
     if (config('test.settings')->get('test_show_extra_text')) {
     	$output .= '<p>extra text turned on by setting</p>';
     }
     return $output;
+  }  
+  
+  public function onKernelResponse(GetResponseEvent $event) {
+    die('finally');
   }
 
 }
