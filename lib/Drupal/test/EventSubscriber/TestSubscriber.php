b0023c8ec4992e1a8c28a58584a95f27b3a1733f
Joshua Taylor <joshua.taylor@acquia.com>
2013-07-10 22:26:33 -0400

    working maintenance200

---
+++ lib/Drupal/test/EventSubscriber/TestSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\mymodule\EventSubscriber\MaintenanceModeSubscriber.
+ */
+
+namespace Drupal\test\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class TestSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Does something.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   *   The event to process.
+   */
+  public function onKernelResponse(FilterResponseEvent $event) {
+  	//echo "<pre>"; var_dump($event); echo "</pre>";
+    $request = $event->getRequest();
+    $response = $event->getResponse();
+    if ($request->attributes->get('_maintenance') == MENU_SITE_OFFLINE) {
+			$response->setStatusCode('200');
+      $event->setResponse($response);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::RESPONSE][] = array('onKernelResponse', 31);
+    return $events;
+  }
+
+}
+
+// }
